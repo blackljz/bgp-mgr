6735c966feb3b5612ce1d7e114abf440fb1d9b13
liangjianze <liangjianze@jd.com>
2018-12-02 21:42:54 +0800

    page

---
+++ src/main/java/com/bgp/mgr/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.bgp.mgr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "/user/userList";
+    }
+}
